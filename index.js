9aea2146123b6c9cea4b9183931d011b209cb453
Mohit Rohilla <118180056+Mohitrohilla9679@users.noreply.github.com>
2023-03-02 15:05:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Mohit Rohilla Success hogya\n'
+  const msg = 'Hello Mohit Rohilla  project has Success \n'
   res.end(msg);
 });
 
